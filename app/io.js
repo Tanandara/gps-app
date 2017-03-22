e8475ed24b008dc183cd4e4071b61e75ebdda2a3
Tanandara <tanandara2014@gmail.com>
2017-03-22 08:26:30 +0700

    complete

--- app/io.js
+++ app/io.js
@@ -1,36 +1,15 @@
 module.exports = function(io) {
-	// var gpslist = [
-	// 	{ id: 'marker2', lat: 14.7510555, long: 100.65721090000001 },
-	// 	{ id: 'marker5', lat: 12.7510555, long: 100.65721090000001 }
-	//
-	// ];
-	var gpslist = [];
 	io.on('connect', function(client) {
 		console.log('Connected : (' + client.id + ')');
 		client.emit('greet', { message: 'Welcome to socket.io' });
 		client.on('room', function(data) {
-			if(gpslist.length == 0) {
-				gpslist = [data];
-			}else if(gpslist.filter((x)=>x.id==data.id).length == 0){
-				gpslist = [...gpslist,data];
-				console.log(gpslist);
-			}else{
-				gpslist.forEach((value,index)=>{
-					if(value.id==data.id) {
-						value.lat = data.lat;
-						value.long = data.long;
-					}
-				})
-				console.log(gpslist);
-			}
-			io.emit('room',  gpslist );
+			data.client = client.id;
+			console.log(data);
+			io.emit('room',  data );
 		});
 		client.on('disconnect', function() {
 			 console.log('Disconnected : (' + client.id +')');
+			 io.emit('disconnect_room',client.id);
 		});
 	});
-
-	// setTimeout(function() {
-	// 	io.emit('greet', { message: 'Hello World' });
-	// }, 2000);
 }
